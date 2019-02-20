a3af99dc7518f6f64f040b862d223ed1422a7afb
unknown <kadeemhaynes45@gmail.com>
2019-02-20 15:36:36 -0400

    add more code

--- client/main.js
+++ client/main.js
@@ -2,21 +2,43 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+import '../lib/collections.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.profiles.helpers({
+	descALL(){
+		return userDB.find({});
+	},
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.profiles.events({
+	'click .js-like'(event, instance) {
+    	console.log("You clicked like");
+	},
+	'click .js-dislike'(event, instance) {
+		console.log("You clicked dislike");
+	},	
+	'click .js-delete' (event, instance) {
+		
+		console.log(this._id);
+		userDB.remove({_id: this._id});
+   }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.addprofile.events({
+	'click .js-save'(event, instance) {
+		var fName = $('#exampleModal input[name="firstName"]').val();
+		var lName = $('#exampleModal input[name="lastName"]').val();
+		var image = $('#exampleModal input[name="imageURL"]').val();
+		console.log("Name: ", fName, lName);
+		console.log("Picture: ", image);
+
+		$('#exampleModal input[name="firstName"]').val('');
+		$('#exampleModal input[name="lastName"]').val('');
+		$('#exampleModal input[name="imageURL"]').val('');
+
+		$('#exampleModal').modal('hide');
+
+		userDB.insert({'firstName':fName, 'lastName':lName, 'img':image});
+
+	},
 });
